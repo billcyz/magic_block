c2817bd5f7b481ff0839f8e15c1d380259ad3af0
billcyz <billcyz@gmail.com>
2017-08-13 16:47:46 +1200

    Allocating required resources for request

---
+++ src/magic_traffic_allocator.erl
@@ -0,0 +1,13 @@
+%% MagicBlock
+%% Data: 13/08/2017
+
+%% --- Code Description ---
+%% Allocating required resources.
+
+-module(magic_traffic_allocator).
+
+-export([]).
+
+%% ===========================================================================
+
+
