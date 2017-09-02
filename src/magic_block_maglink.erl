4eca6a0b36f41cfa4715f0b8420982961fd9e704
billcyz <billcyz@gmail.com>
2017-09-02 18:03:34 +1200

    maglink for magic block system

---
+++ src/magic_block_maglink.erl
@@ -0,0 +1,12 @@
+%% MagicBlock
+%% Data: 2/09/2017
+
+%% --- Code Description ---
+%% Maglink for magic block system
+
+-module(magic_block_maglink).
+
+-export([]).
+
+%% ===========================================================================
+
