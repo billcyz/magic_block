ded0848614b4b8fb24d4a29daeb08fec64769b35
billcyz <billcyz@gmail.com>
2017-10-04 22:55:02 +1300

    traffic router (may not related to this project)

---
+++ src/traffic_manager/traffic_router.erl
@@ -0,0 +1,14 @@
+%% @author billcyz
+%% @doc @todo Add description to traffic_router.
+
+%% Might not related to this project, but
+%% might be a blueprint of future traffic
+%% routing design
+
+-module(traffic_router).
+
+-export([]).
+
+
+
+
