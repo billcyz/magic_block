62eef8ebc0960b7c8ef1ed2da18a36329d9abf64
billcyz <billcyz@gmail.com>
2017-10-26 01:47:02 +1300

    subscription

--- src/traffic_manager/magic_block_subscription.erl
+++ src/traffic_manager/magic_block_subscription.erl
@@ -4,15 +4,43 @@
 
 -module(magic_block_subscription).
 
-%% ====================================================================
-%% API functions
-%% ====================================================================
 -export([]).
 
+-record(state, {}).
+-record(subscription, {subscription_name,
+					   subscription_client,
+					   subscription_option}).
 
 
-%% ====================================================================
-%% Internal functions
-%% ====================================================================
+%% ---------------------------------------------------------------------------
 
+start_link() ->
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
+init([]) ->
+	case init_subtable() of
+		ok -> {ok, #state{}};
+		E -> E
+	end.
+
+%% Create initial subscription ets table
+%% Table stores subscription basic info
+-spec init_subtable() -> 'ok'.
+init_subtable() ->
+	ok.
+
+%% Start subscription service
+-spec start_subscription(atom()) -> 'ok'.
+start_subscription(SubName) ->
+	
+	ok.
+
+%% List subscription service
+-spec list_subscriptions() -> list().
+list_subscriptions() ->
+	1.
+
+%% Add subscription client
+-spec add_subscription_client(atom()) -> 'ok'.
+add_subscription_client(Client) ->
+	1.
